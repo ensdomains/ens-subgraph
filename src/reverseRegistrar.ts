eae5f4bad0a4f50a8d3a5529e1c7a30ccc8a009b
Ed Zynda <ezynda3@gmail.com>
2021-02-02 16:50:00 +0800

    create ReverseRegistration entity

---
+++ src/reverseRegistrar.ts
@@ -0,0 +1,11 @@
+import { SetNameCall } from "./types/ReverseRegistrar/ReverseRegistrar";
+import { Account, ReverseRegistration } from "./types/schema";
+
+export function handleSetName(call: SetNameCall): void {
+    const registration = new ReverseRegistration(call.transaction.hash.toHexString())
+    const account = new Account(call.from.toHexString())
+    account.save()
+    registration.registrant = account.id
+    registration.name = call.inputs.name
+    registration.save()
+}
